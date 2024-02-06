9807e47184529d1c059356a33fa2dbc237b10bcb
Lalit Gour <lalitg@iitbhilai.ac.in>
2024-02-06 16:47:29 +0530

    Delete Accoutn API Added

--- src/routes/publicRoute.js
+++ src/routes/publicRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express();
-import { registerUser, loginUser ,token,user_update,password_update} from '../controllers/user/userController.js';
+import { registerUser, loginUser ,token,user_update,password_update,user_account_del} from '../controllers/user/userController.js';
 import { cart_data_get,cart_data_post,cart_data_del, cart_data_update} from '../controllers/cart/cartController.js';
 import { order_data_get,order_data_post,order_data_del} from '../controllers/order/orderController.js';
 import { product_data_get,product_data_post,product_data_update ,get_all_products} from '../controllers/product/productController.js';
@@ -12,6 +12,7 @@ router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.put('/user_update',user_update);
 router.put('/password_update',password_update)
+router.delete('/user_account_del',user_account_del)
 router.post('/token',token);
 router.get('/cart_data_get',cart_data_get);
 router.post('/cart_data_post',cart_data_post);
